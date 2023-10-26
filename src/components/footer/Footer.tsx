26758fa3027ebc9cda6fe4a697e4210466dfdef0
Tanthiel50 <66637911+Tanthiel50@users.noreply.github.com>
2023-10-26 15:16:36 +0200

    project start+menu

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer =() => {
+    return (
+        <div className="footer">
+            <span>Cara</span>
+            <span>@Cara React Admin Dashboard</span>
+        </div>
+    )
+}
+
+export default Footer
